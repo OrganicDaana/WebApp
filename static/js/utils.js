c7cf08382c4be35e0ea2b93ba0709ae65ece61ef
Sujeet2003 <sujeetsmpatel@gmail.com>
2025-06-05 20:54:37 +0530

    added hero section

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,10 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+const navbar = document.getElementById('mainNavbar');
+
+// Toggle on scroll
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 100) {
+    navbar.classList.add('scrolled');
+    } else {
+    navbar.classList.remove('scrolled');
+    }
+});
+
+// Collapse menu on outside click (for mobile nav)
 document.addEventListener('click', function (event) {
-const navbarToggler = document.querySelector('.navbar-toggler');
-const navbarCollapse = document.querySelector('.navbar-collapse');
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    const isClickInside = navbarCollapse.contains(event.target) || navbarToggler.contains(event.target);
 
-const isClickInsideNavbar = navbarCollapse.contains(event.target) || navbarToggler.contains(event.target);
-
-if (!isClickInsideNavbar && navbarCollapse.classList.contains('show')) {
+    if (!isClickInside && navbarCollapse.classList.contains('show')) {
     new bootstrap.Collapse(navbarCollapse).hide();
-}
+    }
+});
 });
