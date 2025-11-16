185c1d329cc115919a36ad6c53cb8a36901d8dc6
organicdaana <organicdaana@gmail.com>
2025-11-16 16:10:21 +0530

    changes for the cloudflare config

--- AgriSwiftWebApp/settings.py
+++ AgriSwiftWebApp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'BaseApp',
+    'django_distill',  # ← REQUIRED!
 ]
 
 MIDDLEWARE = [
--- BaseApp/urls.py
+++ BaseApp/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from BaseApp import views
+from django_distill import distill_path
 
+
+def get_none():
+    return None
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    distill_path('', views.index, name='index', distill_func=get_none),
 ]
